ea658015291e43dad0fe1a42c65489a532319e00
133226217-myseneca <sislam101@myseneca.ca>
2023-02-15 19:13:49 -0500

    Added helloWorld() function to lab3.py

---
+++ lab3.py
@@ -0,0 +1,11 @@
+def write_name_to_file():
+    with open('my_name.txt', 'w') as file:
+        file.write('Shefayen Islam')
+
+write_name_to_file()
+
+def helloWorld():
+    print('Hello World')
+
+helloWorld()
+
